47effce9656a8162424f7d3353eae585d232dca7
jaynou <93495926+jaynou@users.noreply.github.com>
2022-04-27 19:41:19 -0400

    Create class player and constructor

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+class Player {
+    set name(name) {
+        this.name = name;
+    }
+    set title(title) {
+        this.title = title;
+    }
+    set encouragement(num) {
+        this.encouragement = num;
+    }
+    get name() {
+        return this.name;
+    }
+    get title() {
+        return this.title;
+    }
+    get encouragement() {
+        return this.encouragement
+    }
 
+    constructor(name) {
+        this.name = name;
+        this.title = title;
+    }
+}
+
+let p1 = new Player();
+console.log(p1.name)
