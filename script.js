92ccd84417c1880bd426275f7124a37d8c714759
Mars Brown <meeshajean33@gmail.com>
2022-04-27 20:22:29 -0400

    idea of how to proceed

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function getAllInfo() {
+  meterDetails();
+  return (
+    "Name:" +
+    this.name +
+    "\n" +
+    "Title:" +
+    this.title +
+    "\n" +
+    "Health:" +
+    this.health +
+    "\n"
+  );
+}
 
+function meterDetails() {
+  // Returns the Encouragement Level
+  if (parseInt(this._health) < 50) {
+    return "frowny face by meter" + "I'm feeling sad display in html";
+  } else {
+    return "happy face by meter" + "I'm feeling happy display in html";
+  }
+}
