983b69c352a7faa0c30bd711c06282e371d35eaa
Mars Brown <meeshajean33@gmail.com>
2022-04-29 20:34:09 -0400

    added meter to js

--- script.js
+++ script.js
@@ -3,44 +3,41 @@
 //const playerTwoName = document.getElementById("name");
 const emoji = document.getElementById("#emoji");
 const imFeeling = document.getElementById("#imFeeling");
+const encouragementMeter = document.getElementById("#meter");
 
 class Player {
-    set name(name) {
-        this.name = name;
-    }
-    set title(title) {
-        this.title = title;
-    }
-    set encouragementMeter(num) {
-        this.encouragementMeter = num;
-    }
-    get name() {
-        return this.name;
-    }
-    get title() {
-        return this.title;
-    }
-    get encouragement() {
-        return this.encouragementMeter
-    }
-
-    constructor(name,title) {
-        this.name = name;
-        this.title = title;
-    }
-
-
-    
-//reset 
-reset(){
-  this.name = "";
-  this.encouragementMeter = 0;
-  this.encouragementBonus = "";
+  set name(name) {
+    this.name = name;
+  }
+  set title(title) {
+    this.title = title;
+  }
+  set encouragementMeter(num) {
+    this.encouragementMeter = num;
+  }
+  get name() {
+    return this.name;
+  }
+  get title() {
+    return this.title;
+  }
+  get encouragement() {
+    return this.encouragementMeter;
   }
 
+  constructor(name, title) {
+    this.name = name;
+    this.title = title;
+  }
 
+  //reset
+  reset() {
+    this.name = "";
+    this.encouragementMeter = 0;
+    this.encouragementBonus = "";
+  }
 }
-
+encouragementMeter = this.encouragementMeter;
 
 function getAllInfo() {
   meterDetails();
@@ -51,8 +48,7 @@ function getAllInfo() {
     "Title:" +
     this.title +
     "\n" +
-    "encouragementMeter:" +
-    this.encouragementMeter +
+    encouragementMeter +
     "\n"
   );
 }
@@ -66,10 +62,10 @@ function meterDetails() {
     parseInt(this._encouragementMeter) < 75 &&
     parseInt(this._encouragementMeter) > 50
   ) {
-     emoji.innerHTML = "😉";
-     imFeeling.innerHTML = "I'm feeling good";
+    emoji.innerHTML = "😉";
+    imFeeling.innerHTML = "I'm feeling good";
   } else {
-     emoji.innerHTML = "😁";
-     imFeeling.innerHTML = "I'm feeling great";
+    emoji.innerHTML = "😁";
+    imFeeling.innerHTML = "I'm feeling great";
   }
 }
