f7e58965f48f87382e578b44fd31c392660c9842
Rikki Crayton <rikkicrayton@Rikkis-Air.attlocal.net>
2022-05-04 20:51:30 -0400

    Added function to add points based on ability

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-<<<<<<< HEAD
+
 //example of where to do dom links
 //const playerOneName = document.getElementById("name");
 //const playerTwoName = document.getElementById("name");
 //DOM Elements
 let ability_array = ["Gift", "Lunch", "Coffee", "Walk", "Chocolate", "Inspire"];
+
 
 const emoji = document.getElementById("#emoji");
 const imFeeling = document.getElementById("#imFeeling");
@@ -13,25 +14,178 @@ const p2_feelings = document.getElementById("playerTwo_Feelings");
 const p2_meter = document.getElementById("playerTwo_MeterNumber");
 
 const p1_btn1 = document.getElementById("playerOne_ButtonOne"),
-   p1_btn2 = document.getElementById("playerOne_ButtonTwo"),
-   p1_btn3 = document.getElementById("playerOne_ButtonThree"),
-   p2_btn1 = document.getElementById("playerTwo_ButtonOne"),
-   p2_btn2 = document.getElementById("playerTwo_ButtonTwo"),
-   p2_btn3 = document.getElementById ("playerTwo_ButtonThree");
+  p1_btn2 = document.getElementById("playerOne_ButtonTwo"),
+  p1_btn3 = document.getElementById("playerOne_ButtonThree"),
+  p2_btn1 = document.getElementById("playerTwo_ButtonOne"),
+  p2_btn2 = document.getElementById("playerTwo_ButtonTwo"),
+  p2_btn3 = document.getElementById ("playerTwo_ButtonThree");
 
-   
-p1_name.textContent = current_player_1.name;
-p1_title.textContent = current_player_1.title;
+
+  class Player {
+  // set name(name) {
+  //   this.name = name;
+  // }
+  // set title(title) {
+  //   this.title = title;
+  // }
+  // set encouragementMeter(num) {
+  //   this.encouragementMeter = num;
+  // }
+  // get name() {
+  //   return this.name;
+  // }
+  // get title() {
+  //   return this.title;
+  // }
+  // get encouragement() {
+  //   return this.encouragementMeter;
+  // }
+
+  // constructor(name, title) {
+  //   this.name = name;
+  //   this.title = title;
+  // }
+
+  //reset
+  reset() {
+    this.name = "";
+    this.encouragementMeter = 100;
+    this.encouragementBonus = "";
+  }
+}
+
+function getAllInfo() {
+  meterDetails();
+  return (
+    "Name:" +
+    this.name +
+    "\n" +
+    "Title:" +
+    this.title +
+    "\n" +
+    "encouragementMeter:" +
+    this.encouragementMeter +
+    "\n"
+  );
+}
+
+//meter functions
+function meterDetails() {
+  // Returns the Encouragement Level
+  if (parseInt(this._encouragementMeter) < 50) {
+    emoji.innerHTML = "☹️";
+    imFeeling.innerHTML = "I'm feeling sad";
+  } else if (
+    parseInt(this._encouragementMeter) < 75 &&
+    parseInt(this._encouragementMeter) > 50
+  ) {
+    emoji.innerHTML = "😉";
+    imFeeling.innerHTML = "I'm feeling good";
+  } else {
+    emoji.innerHTML = "😁";
+    imFeeling.innerHTML = "I'm feeling great";
+  }
+  p1_meter.textContent =
+    parseInt(p1_meter.textContent) + parseInt(givePoints(current_abilty));
+  p2_meter.textContent =
+    parseInt(p2_meter.textContent) + parseInt(givePoints(current_abilty));
+}
+
+
+
+//player objects
+let filmon = new Player("Filmon", "UX Engineer");
+let erica = new Player("Erica", "Fullstack JS");
+let rikki = new Player("Rikki","Fullstack JS");
+let mars = new Player("Mars Brown", "Fullstack JS");
+let cam = new Player("Cam Quevedo", "Fullstack JS");
+let jenny = new Player("Jenny Nou", "Software Engineer");
+let dravon = new Player("Dravon Tyson", "Product Manager");
+let alvian = new Player("Alvian Williams", "Product Manager");
+let lucas = new Player("Lucas Rangel", "Fullstack Engineer");
+let khris = new Player("Khris Goins", "Freelance Front-End Engineer");
+let zakiyah = new Player("Zakiyah Muhammad", "Front-End Developer");
+let isaac = new Player("Isaac Porter", "Software Engineer");
+let tuerei = new Player("Tuerei Williams", "Product Manager");
+let player_array = [
+  filmon,
+  erica,
+  mars,
+  cam,
+  dravon,
+  jenny,
+  rikki,
+  lucas,
+  alvian,
+  khris,
+  zakiyah,
+  isaac,
+  tuerei,
+];
+//
+let randomNumber;
+let current_player_1;
+let current_player_2;
+
+//randomize player cards
+function chooseRandomPlayers() {
+  randomNumber = Math.floor(Math.random() * player_array.length);
+  current_player_1 = player_array[randomNumber];
+  player_array.splice(randomNumber, 1);
+  current_player_2 = player_array[randomNumber];
+  return randomNumber;
+}
+
+chooseRandomPlayers();
+
+function givePoints(ability){
+
+  switch (ability) {
+    case "Gift":
+      ability_points = 7;
+      return ability_points;
+
+    case "Coffee":
+      ability_points = 4;
+      return ability_points;
+
+    case "Walk":
+      ability_points = 3;
+      return ability_points;
+
+    case "Chocolate":
+      ability_points = 13;
+      return ability_points;
+
+    case "Inspire":
+      ability_points = 9;
+      return ability_points;
+
+    case "Lunch":
+      ability_points = 15;
+      return ability_points;
+
+    default:
+      ability_points = 0;
+      break;
+  }
+
+}
+
+
+// p1_name.textContent = current_player_1.name;
+// p1_title.textContent = current_player_1.title;
 p1_btn1.addEventListener("click", p1_btn1_clicked);
 p1_btn2.addEventListener("click", p1_btn2_clicked);
 p1_btn3.addEventListener("click", p1_btn3_clicked);
 
-pla_btn1.addEventListener("click", p2_btn1_clicked);
-playerTwo_btn2.addEventListener("click", p2_btn2_clicked);
-playerTwo_btn3.addEventListener("click", p2_btn3_clicked);
+p2_btn1.addEventListener("click", p2_btn1_clicked);
+p2_btn2.addEventListener("click", p2_btn2_clicked);
+p2_btn3.addEventListener("click", p2_btn3_clicked);
 
 function p1_btn1_clicked(e){
-  console.log(e.target.textContent);
+  console.log("I'm working");
+    // e.target.textContent);
   current_ability = e.target.textContent;
   console.log(givePoints(current_ability));
   p2_meter.textContent = parseInt(p2_meter.textContent) + parseInt(givePoints(current_ability));
@@ -131,146 +285,6 @@ function p2_btn3_clicked(e){
   randomAbilities();
 
 }
-
-class Player {
-    set name(name) {
-        this.name = name;
-    }
-    set title(title) {
-        this.title = title;
-    }
-    set encouragementMeter(num) {
-        this.encouragementMeter = num;
-    }
-    get name() {
-        return this.name;
-    }
-    get title() {
-        return this.title;
-    }
-    get encouragement() {
-        return this.encouragementMeter
-    }
-
-    constructor(name) {
-        this.name = name;
-        this.title = title;
-    }
-
-
-
-class Player {
-  set name(name) {
-    this.name = name;
-  }
-  set title(title) {
-    this.title = title;
-  }
-  set encouragementMeter(num) {
-    this.encouragementMeter = num;
-  }
-  get name() {
-    return this.name;
-  }
-  get title() {
-    return this.title;
-  }
-  get encouragement() {
-    return this.encouragementMeter;
-  }
-
-  constructor(name, title) {
-    this.name = name;
-    this.title = title;
-  }
-
-  //reset
-  reset() {
-    this.name = "";
-    this.encouragementMeter = 100;
-    this.encouragementBonus = "";
-  }
-}
-
-function getAllInfo() {
-  meterDetails();
-  return (
-    "Name:" +
-    this.name +
-    "\n" +
-    "Title:" +
-    this.title +
-    "\n" +
-    "encouragementMeter:" +
-    this.encouragementMeter +
-    "\n"
-  );
-}
-
-//meter functions
-function meterDetails() {
-  // Returns the Encouragement Level
-  if (parseInt(this._encouragementMeter) < 50) {
-    emoji.innerHTML = "☹️";
-    imFeeling.innerHTML = "I'm feeling sad";
-  } else if (
-    parseInt(this._encouragementMeter) < 75 &&
-    parseInt(this._encouragementMeter) > 50
-  ) {
-    emoji.innerHTML = "😉";
-    imFeeling.innerHTML = "I'm feeling good";
-  } else {
-    emoji.innerHTML = "😁";
-    imFeeling.innerHTML = "I'm feeling great";
-  }
-  p1_meter.textContent =
-    parseInt(p1_meter.textContent) + parseInt(givePoints(current_abilty));
-  p2_meter.textContent =
-    parseInt(p2_meter.textContent) + parseInt(givePoints(current_abilty));
-}
-
-//player objects
-let filmon = new Player("Filmon", "UX Engineer");
-let mars = new Player("Mars Brown", "Fullstack JS");
-let cam = new Player("Cam Quevedo", "Fullstack JS");
-let jenny = new Player("Jenny Nou", "Software Engineer");
-let dravon = new Player("Dravon Tyson", "Product Manager");
-let alvian = new Player("Alvian Williams", "Product Manager");
-let lucas = new Player("Lucas Rangel", "Fullstack Engineer");
-let khris = new Player("Khris Goins", "Freelance Front-End Engineer");
-let zakiyah = new Player("Zakiyah Muhammad", "Front-End Developer");
-let isaac = new Player("Isaac Porter", "Software Engineer");
-let tuerei = new Player("Tuerei Williams", "Product Manager");
-let player_array = [
-  filmon,
-  erica,
-  mars,
-  cam,
-  dravon,
-  jenny,
-  rikki,
-  lucas,
-  alvian,
-  khris,
-  zakiyah,
-  isaac,
-  tuerei,
-];
-//
-let randomNumber;
-let current_player_1;
-let current_player_2;
-
-//randomize player cards
-function chooseRandomPlayers() {
-  randomNumber = Math.floor(Math.random() * player_array.length);
-  current_player_1 = player_array[randomNumber];
-  player_array.splice(randomNumber, 1);
-  current_player_2 = player_array[randomNumber];
-  return randomNumber;
-}
-
-chooseRandomPlayers();
 
 //randomize abilities
 function randomAbilities() {
